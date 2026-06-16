c7e22262a70d4ab0555963bdaf028b34b00139aa
ls220913 <lisai220913@163.com>
2026-06-16 19:43:40 +0800

    Update usb_conf.h

--- USB_hid/usb_conf.h
+++ USB_hid/usb_conf.h
@@ -1,17 +1,4 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : usb_conf.h
-* Author             : MCD Application Team
-* Version            : V2.2.1
-* Date               : 09/22/2008
-* Description        : Custom HID demo configuration file
-********************************************************************************
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*******************************************************************************/
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __USB_CONF_H
@@ -89,5 +76,5 @@
 
 #endif /*__USB_CONF_H*/
 
-/******************* (C) COPYRIGHT 2008 STMicroelectronics *****END OF FILE****/
+
 
