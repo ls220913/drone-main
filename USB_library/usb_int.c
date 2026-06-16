e67546ab1436bf6bed9b5e3b505946e04c8b60f2
ls220913 <lisai220913@163.com>
2026-06-16 19:47:02 +0800

    Update usb_int.c

--- USB_library/usb_int.c
+++ USB_library/usb_int.c
@@ -1,17 +1,4 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : usb_int.c
-* Author             : MCD Application Team
-* Version            : V2.2.1
-* Date               : 09/22/2008
-* Description        : Endpoint CTR (Low and High) interrupt's service routines
-********************************************************************************
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*******************************************************************************/
+
 
 /* Includes ------------------------------------------------------------------*/
 #include "usb_lib.h"
@@ -188,5 +175,3 @@ void CTR_HP(void)
 
   }/* while(...) */
 }
-
-/******************* (C) COPYRIGHT 2008 STMicroelectronics *****END OF FILE****/
