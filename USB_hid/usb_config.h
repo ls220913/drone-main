2a2b5708c03b649a8a50f302490dd914d41f9bae
ls220913 <lisai220913@163.com>
2026-06-16 19:44:16 +0800

    Update usb_config.h

--- USB_hid/usb_config.h
+++ USB_hid/usb_config.h
@@ -1,17 +1,4 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : hw_config.h
-* Author             : MCD Application Team
-* Version            : V2.2.1
-* Date               : 09/22/2008
-* Description        : Hardware Configuration & Setup
-********************************************************************************
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*******************************************************************************/
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __HW_CONFIG_H
@@ -39,4 +26,3 @@ void Get_SerialNum(void);
 void USB_Connect (bool con);
 #endif  /*__HW_CONFIG_H*/
 
-/******************* (C) COPYRIGHT 2008 STMicroelectronics *****END OF FILE****/
