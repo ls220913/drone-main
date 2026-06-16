c79dafd056cf3f8114fa82ebec68025023d9eae6
ls220913 <lisai220913@163.com>
2026-06-16 19:44:42 +0800

    Update usb_desc.h

--- USB_hid/usb_desc.h
+++ USB_hid/usb_desc.h
@@ -1,17 +1,4 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : usb_desc.h
-* Author             : MCD Application Team
-* Version            : V2.2.1
-* Date               : 09/22/2008
-* Description        : Descriptor Header for Custom HID Demo
-********************************************************************************
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*******************************************************************************/
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __USB_DESC_H
@@ -54,4 +41,4 @@ extern u8 CustomHID_StringSerial[CUSTOMHID_SIZ_STRING_SERIAL];
 
 #endif /* __USB_DESC_H */
 
-/******************* (C) COPYRIGHT 2008 STMicroelectronics *****END OF FILE****/
+
